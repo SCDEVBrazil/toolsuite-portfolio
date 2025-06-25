a1d4752625b49dbc51a7a6584fdb0743cd8ace9d
Renan Teixeira <ceo@accelerateai.ai>
2025-06-25 11:01:26 -0500

    Fix Google Analytics SSR issues causing Googlebot 404 errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 
 // Google Analytics tracking ID
 const GA_TRACKING_ID = 'G-GMB3FLKZFC'
@@ -13,17 +14,21 @@ const gtag = (...args) => {
 }
 
 const pageview = (url) => {
-  gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  })
+  if (typeof window !== 'undefined') {
+    gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+    })
+  }
 }
 
 const event = ({ action, category, label, value }) => {
-  gtag('event', action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  })
+  if (typeof window !== 'undefined') {
+    gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    })
+  }
 }
 
 export default function App({ Component, pageProps }) {
@@ -42,7 +47,24 @@ export default function App({ Component, pageProps }) {
     }
   }, [router.events])
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 // Export the event function for use in other components
